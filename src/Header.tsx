5679946754f513fc3149028854eda0481d4f4a6c
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-30 02:54:09 -0700

    additionally add useNavigate Hook

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,27 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+
+
 
 export default function Header() {
+
+    const nav = useNavigate();
+    function navigateHome() {
+        nav("/")
+    }
+    function navigateCharacters() {
+        nav("/characters")
+    }
+
     return (
         <>
-            <h1>Rick & Morty Gallery</h1>
+            <h3>Navigation Bar</h3>
             <Link to={"/"}>Home Page </Link>
             <Link to={"/characters"}> Characters</Link>
+
+            <h3>UseNavigate Hook</h3>
+            <button onClick={navigateHome}>Home Page</button>
+            <button onClick={navigateCharacters}>Characters</button>
+
         </>
     )
 }
