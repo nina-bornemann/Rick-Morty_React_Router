c0a420ea55d1ebfa582f2dd514751130288c8966
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-30 05:56:50 -0700

    add submit function to add new character

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,4 +1,7 @@
-import {useState} from "react";
+import {FormEvent, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {Character} from "./types/RickAndMortyCharacter.ts";
+import {characters} from "./Characters.ts";
 
 export default function Create() {
 
@@ -6,13 +9,21 @@ export default function Create() {
     const [species, setSpecies] = useState<string>("");
     const [status, setStatus] = useState<string>("");
 
+    const nav = useNavigate()
+
+    function handleSubmit(e:FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const newCharacter:Character = {name: name, species: species, status: status} as Character
+        characters.push(newCharacter);
+        nav("/characters")
+    }
 
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Name:
                     <input type={"text"} placeholder={"Enter Name"}
-                           onChange={(e) =>setName(e.target.value)}/>
+                           onChange={(e) => setName(e.target.value)}/>
                 </label>
                 <label>Species:
                     <input type={"text"} placeholder={"Enter Species"}
@@ -22,6 +33,8 @@ export default function Create() {
                     <input type={"text"} placeholder={"Enter Status"}
                     onChange={e => setStatus(e.target.value)}/>
                 </label>
+
+                <button type={"submit"}>Add new Character</button>
             </form>
         </>
     )
